f1243b6345c15cc1b3079158ac82379691c3a104
Hua Xia <rednik111@gmail.com>
2016-02-05 20:39:38 -0800

    Create switcher_ENG.sh

---
+++ switcher_ENG.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#********************************************************************
+# Simple Script to turn on or off Macbook internal keyboard
+#                           v1.0.0
+# Author: redniktony                    e-mail :rednik111@gmail.com
+#********************************************************************
+echo " "
+echo " "
+echo " "
+echo "********************************************************************"
+echo "                  Macbook Keyboard Switcher v1.0.0"
+echo "                              By tonyrednik"
+echo "                                 WARNING！"
+echo "＊＊PLEASE USE THIS PROGRAM WHILE YOU HAVE YOUR OWN OUTSIDE KEYBOARD＊＊"
+echo "＊＊PLEASE USE THIS PROGRAM WHILE YOU HAVE YOUR OWN OUTSIDE KEYBOARD＊＊"
+echo "＊＊PLEASE USE THIS PROGRAM WHILE YOU HAVE YOUR OWN OUTSIDE KEYBOARD＊＊"
+echo "            VERY IMPORTANT, UNLESS YOU WANNA RESTART YOUR COMPUTER"
+echo "********************************************************************"
+
+echo "IF YOU SURE YOU ALL SET, PLEASE PRESS ENTER："
+read
+
+sudo kextunload /System/Library/Extensions/AppleUSBTopCase.kext/Contents/PlugIns/AppleUSBTCKeyboard.kext/ || sudo kextload /System/Library/Extensions/AppleUSBTopCase.kext/Contents/PlugIns/AppleUSBTCKeyboard.kext/
+
+echo " "
+echo $?
+echo "SUCCESS THANKS FOR USINGψ(｀∇´)ψ"
+echo "To turn on your keyboard, just need to run this program again"
+echo " "
+echo " "
